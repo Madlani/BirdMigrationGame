fa84181b56610256c1071f882b62e4238ed5eb6a
bbugieda <brian.bugieda@gmail.com>
2019-04-15 13:52:35 -0400

    added new test cases in ModelTest.java

--- BirdGameV1/src/tests/ModelTest.java
+++ BirdGameV1/src/tests/ModelTest.java
@@ -26,4 +26,65 @@ class ModelTest {
 		assertEquals(false, test.detectCollisions());
 		assertFalse(test.detectCollisions());
 	}
+	
+	@Test
+	public void testGetFrameHeight() {
+		Model test = new Model(5, 4, 3, 2);
+		assertEquals(true, test.getFrameHeight() == 4);
+	}
+	
+	@Test
+	public void testSetFrameHeight() {
+		Model test = new Model(5, 4, 3, 2);
+		test.setFrameHeight(10);
+		assertEquals(true, test.getFrameHeight() == 10);
+	}
+	
+	@Test
+	public void testSetFrameWidth() {
+		Model test = new Model(5, 4, 3, 2);
+		test.setFrameWidth(20);
+		assertEquals(true, test.getFrameWidth() == 20);
+	}
+	
+	@Test
+	public void testGetImgHeight() {
+		Model test = new Model(5, 4, 3, 2);
+		test.setImgHeight(20);
+		assertEquals(true, test.getImgHeight() == 20);
+	}
+	
+	@Test
+	public void testSetImgHeight() {
+		Model test = new Model(5, 4, 3, 2);
+		test.setImgHeight(20);
+		assertEquals(true, test.getImgHeight() == 20);
+	}
+	
+	@Test
+	public void testGetImgWidth() {
+		Model test = new Model(5, 4, 3, 2);
+		test.setImgWidth(20);
+		assertEquals(true, test.getImgWidth() == 20);
+	}
+	
+	@Test
+	public void testSetImgWidth() {
+		Model test = new Model(5, 4, 3, 2);
+		test.setImgWidth(20);
+		assertEquals(true, test.getImgWidth() == 20);
+	}
+	
+	@Test
+	public void testGetXVector() {
+		Model test = new Model(5, 4, 3, 2);
+		test.setxVector(8);
+		assertEquals(true, test.getxVector() == 8);
+	}
+	
+	@Test public void testSetXVector() {
+		Model test = new Model(5, 4, 3, 2);
+		test.setxVector(8);
+		assertEquals(true, test.getxVector() == 8);
+	}
 }
