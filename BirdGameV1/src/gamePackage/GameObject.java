a19a3882e8c32730ad8d70ea705442198f7d956b
bbugieda <brian.bugieda@gmail.com>
2019-05-07 20:19:04 -0400

    updated comments in GameObject.java and removed unnecessary code

--- BirdGameV1/src/gamePackage/GameObject.java
+++ BirdGameV1/src/gamePackage/GameObject.java
@@ -1,23 +1,21 @@
 package gamePackage;
-import java.awt.Toolkit;
 import java.awt.geom.Point2D;
 
 public class GameObject extends Point2D {
-	private final int SPEED = 15;
 	protected HitBox GameObjectBox;
-	private double xPosition;
-	private double yPosition;
-	final int imgWidth = 100;
-	
 	private ObjectType type;
 	
+	private double xPosition;
+	private double yPosition;
+	
+	private final int SPEED = 15;
+	
 	public GameObject(double startingX, ObjectType t, int imgWidth, int imgHeight){
-		xPosition = startingX;
+		this.xPosition = startingX;
 		this.type = t;
-		
 		this.GameObjectBox = new HitBox((int)this.getX(), (int)this.getY(), imgWidth, imgHeight);
 		
-		int maxWidth = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth()- this.GameObjectBox.width;
+		int maxWidth = Model.scaledImageWidth - this.GameObjectBox.width;
 		int minWidth = 0;
 		
 		int randY = (int)(Math.random() * (maxWidth - minWidth + 1) + minWidth);
@@ -25,33 +23,50 @@ public class GameObject extends Point2D {
 		this.yPosition = randY;
 	}
 	
-
+	/**
+	 * getX()
+	 * This method overrides the Point2D getX() method
+	 * It returns the current x-position of the GameObject.
+	 */
 	@Override
 	public double getX() {
 		return this.xPosition;
 	}
 
+	/**
+	 * getY()
+	 * This method overrides the Point2D getY() method
+	 * It returns the current y-position of the GameObject.
+	 */
 	@Override
 	public double getY() {
-		// TODO Auto-generated method stub
 		return this.yPosition;
 	}
 	
-	public void scroll() {
-		this.setLocation(this.getX() - SPEED, this.getY());
-	}
-	
+	/**
+	 * setLocation()
+	 * This method overrides the Point2D setLocation() method.
+	 * It sets the x-position and y-position of the GameObject.
+	 */
 	@Override
 	public void setLocation(double x, double y) {
 		this.xPosition = x;
 		this.yPosition = y;
 	}
 	
+	/**
+	 * getGameObjectSpeed()
+	 * @return SPEED, the final GameObject speed
+	 */
 	public int getGameObjectSpeed() {
 		return this.SPEED;
 	}
 
-
+	/**
+	 * getType()
+	 * 
+	 * @return type, the GameObject type
+	 */
 	public ObjectType getType() {
 		return type;
 	}
