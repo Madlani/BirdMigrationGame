2714ca47365b241aa90a374fb2ee40302d17dacc
bbugieda <brian.bugieda@gmail.com>
2019-05-07 20:09:22 -0400

    added comments to the EndView.java class and cleaned up code, created finals for magic numbers

--- BirdGameV1/src/gamePackage/EndView.java
+++ BirdGameV1/src/gamePackage/EndView.java
@@ -3,51 +3,51 @@ package gamePackage;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Toolkit;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 
 @SuppressWarnings("serial")
-
-//display the final state of the game
 public class EndView extends View {
-	
-private Image g1;
-	
-	public int imgVelX = 0;
-	private int scaledImageWidth = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-	private int scaledImageHeight = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-	
+
+	private Image backgroundImage;
+
+	private final int NO_SPEED = 0;
+	private int scaledImageWidth = Model.scaledImageWidth;
+	private int scaledImageHeight = Model.scaledImageHeight;
+
 	public EndView() {
 		super();
 		this.loadImage();
 	}
 
-	public void replayGame() {
-
-	}
-	
-	@Override
-	public void update(ArrayList<GameObject> list) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+	/**
+	 * loadImage()
+	 * Loads the image for the end view
+	 */
 	private void loadImage() {
-		ImageIcon startScreen = new ImageIcon("src/images/endView.png");
-		g1 = startScreen.getImage().getScaledInstance(scaledImageWidth, scaledImageHeight, Image.SCALE_DEFAULT);
-
+		ImageIcon endImage = new ImageIcon("src/images/endView.png");
+		backgroundImage = endImage.getImage().getScaledInstance(scaledImageWidth, scaledImageHeight, Image.SCALE_DEFAULT);
 	}
-	
+
+	/**
+	 * paintComponent()
+	 * Paints the EndView component into the window. This view does not move, so the x and y 
+	 * velocity are filled with our constant NO_SPEED which is 0.
+	 */
 	@Override
 	public void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g.create();
-		g2.drawImage(g1, (imgVelX % scaledImageWidth), 0, null); // draws image in the window
+		g = (Graphics2D) g.create();
+		g.drawImage(backgroundImage, NO_SPEED, NO_SPEED, null);
+	}
+	
+	/**
+	 * update()
+	 * This method is not used in this particular view, but is required to be implemented since we extend the abstract View class.
+	 */
+	@Override
+	public void update(ArrayList<GameObject> list) {
 
 	}
 
 }
-
-
-
