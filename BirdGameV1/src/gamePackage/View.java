8b9a30975e9c4fb88eabfd42be8441ff4d8e6f74
bbugieda <brian.bugieda@gmail.com>
2019-05-13 01:00:49 -0400

    added comments in the View class

--- BirdGameV1/src/gamePackage/View.java
+++ BirdGameV1/src/gamePackage/View.java
@@ -26,10 +26,20 @@ public abstract class View extends JPanel {
 		this.setVisible(true);
 	}
 	
+	/**
+	 * drawBackground()
+	 * Draws the background for the view.
+	 */
 	public void drawBackground() {
 		
 	}
 	
+	/**
+	 * createImage()
+	 * Creates the image that is passed into this method.
+	 * @param name, the file path for the image to be created
+	 * @return a BufferedImage of the image.
+	 */
 	protected BufferedImage createImage(String name) {
 		
 		BufferedImage bufferedImage;
@@ -42,30 +52,65 @@ public abstract class View extends JPanel {
 		return null;
 	}
 	
+	/**
+	 * paintComponent()
+	 * Paints the view component when called.
+	 * @param g
+	 */
 	public void paintComponent(Graphics2D g) {
 		
 	}
 	
+	/**
+	 * getPreferredSize()
+	 * Overrides the JPanel's getPreferredSize()
+	 * @return a new Dimension.
+	 */
 	@Override
 	public Dimension getPreferredSize() {
 		return new Dimension();
 	}
 
+	/**
+	 * update()
+	 * Abstract method which must be implemented in any view classes extending this View.
+	 * @param list, an ArrayList containing the GameObjects
+	 */
 	public abstract void update(ArrayList<GameObject> list);
 	
+	/**
+	 * getBackground()
+	 * @return the color of the background
+	 */
 	@Override
 	public Color getBackground() {
 		return background;
 	}
 
+	/**
+	 * setBackground()
+	 * Sets the background to the one provided in the parameter.
+	 * @param background, the color of the background
+	 */
 	@Override
 	public void setBackground(Color background) {
 		this.background = background;
 	}
 	
+	/**
+	 * getMovement()
+	 * Returns the string representation of the movement.
+	 * @return the movement.
+	 */
 	public String getMovement() {
 		return this.movement;
 	}
+	
+	/**
+	 * setMovement()
+	 * Sets the string representation of the movement to be the one specified in the parameter.
+	 * @param movement
+	 */
 	public void setMovement(String movement) {
 		this.movement = movement;
 	}
